4559c6e9d966df902088e186cb8a9b41a34f9cdf
Maimoon <maimoon.ideal@gmail.com>
2026-06-05 03:34:52 +0500

    refactor: reorder BioSectionAdmin registration in admin panel

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
 from .models import HeroSection, BioSection, JourneyPhase
 
-
-@admin.register(BioSection)
-class BioSectionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'subtitle', 'is_active')
-
-
 @admin.register(HeroSection)
 class HeroSectionAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_active', 'created_at')
     list_filter = ('is_active',)
 
+
+@admin.register(BioSection)
+class BioSectionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtitle', 'is_active')
 
 
 @admin.register(JourneyPhase)
