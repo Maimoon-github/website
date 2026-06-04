d3365bf4bd63f78966da017674a59a74de5117db
Maimoon <maimoon.ideal@gmail.com>
2026-06-05 03:17:35 +0500

    feat: update next.config.ts to allow additional dev origins and media hostnames for hero image support

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['192.168.100.12', 'localhost:3000'],
   images: {
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
+        port: '8000',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
         port: '8000',
         pathname: '/media/**',
       },
